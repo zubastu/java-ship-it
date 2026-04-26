8a591a793f2b3ccc7d86a860395823600186a4b6
zubastu <79757684+zubastu@users.noreply.github.com>
2026-04-26 20:44:44 +0300

    добавил тесты по 3 на каждый класс и по одному пограничному

--- src/ru/yandex/practicum/delivery/DeliveryApp.java
+++ src/ru/yandex/practicum/delivery/DeliveryApp.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Scanner;
 
 public class DeliveryApp {
-
     private static final Scanner scanner = new Scanner(System.in);
     private static List<Parcel> allParcels = new ArrayList<>();
     private static List<Trackable> trackableParcels = new ArrayList<>();
